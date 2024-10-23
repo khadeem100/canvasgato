8aa88759d5423c25a2204e490a1b5bc10f2cea5a
khadeem100 <khadeem.smart@outlook.com>
2024-10-23 14:34:22 +0200

    colorpush

--- src/app/(dashboard)/banner.tsx
+++ src/app/(dashboard)/banner.tsx
@@ -34,13 +34,13 @@ export const Banner = () => {
     <div className="text-white aspect-[5/1] min-h-[248px] flex gap-x-6 p-6 items-center rounded-xl bg-gradient-to-r from-[#2e62cb] via-[#0073ff] to-[#3faff5]">
       <div className="rounded-full size-28 items-center justify-center bg-white/50 hidden md:flex">
         <div className="rounded-full size-20 flex items-center justify-center bg-white">
-          <Sparkles className="h-20 text-[#0073ff] fill-[#0073ff]" />
+          <Sparkles className="h-20 text-[#ff7300] fill-[#ff7b00]" />
         </div>
       </div>
       <div className="flex flex-col gap-y-2">
-        <h1 className="text-xl md:text-3xl font-semibold">Visualize your ideas with The Canvas</h1>
+        <h1 className="text-xl md:text-3xl font-semibold">Visualize your your design</h1>
         <p className="text-xs md:text-sm mb-2">
-          Turn inspiration into design in no time. Simply upload an image and let AI do the rest.
+          Create your design with our own Designer
         </p>
         <Button
           disabled={mutation.isPending}
